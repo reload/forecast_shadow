1aa3c459d8334e063ba7aa4cd2f54bc4e5b0e93a
Kasper Garnaes <kasper.garnaes@gmail.com>
2016-03-03 11:49:42 +0100

    Reformat code to improve legibility

--- shadow.js
+++ shadow.js
@@ -13,37 +13,39 @@ var shadowJss =
         "});\n" +
         "}\n" +
 "}\n" +
-        "/* Adding onClick handler only works when the element exists. This function makes sure the handler is not set, before +\n" +
-         "* the control-button exists.\n" +
-         "**/\n" +
-        "function viewportNavigationExists() {\n" +
-            "if (jQuery('.row-expand-icon').length == 0) {\n" +
-                "setTimeout(viewportNavigationExists, 800);\n" +
-            "}\n" +
-            "else {\n" +
-                "jQuery('.row-expand-icon').click(function () {\n" +
-                    "doShadow();\n" +
-                "});\n" +
-                "doShadow();\n" +
-            "}\n" +
-        "}\n" +
+"/**\n" +
+" * Adding onClick handler only works when the element exists.\n" +
+" * This function makes sure the handler is not set, before +\n" +
+" * the control-button exists.\n" +
+" */\n" +
+"function viewportNavigationExists() {\n" +
+    "if (jQuery('.row-expand-icon').length == 0) {\n" +
+        "setTimeout(viewportNavigationExists, 800);\n" +
+    "}\n" +
+    "else {\n" +
+        "jQuery('.row-expand-icon').click(function () {\n" +
+            "doShadow();\n" +
+        "});\n" +
+        "doShadow();\n" +
+    "}\n" +
+"}\n" +
+"/* Add event handlers */\n" +
+"jQuery(document).ready(function () {\n" +
+    "viewportNavigationExists();\n" +
+"});\n" +
+"jQuery(window).click(function () {\n" +
+    "doShadow();\n" +
+"});\n" +
+"jQuery(window).scroll(function () {\n" +
+    "doShadow();\n" +
+"});\n" +
+"jQuery(window).keydown(function () {\n" +
+    "doShadow();\n" +
+"});\n" +
+"jQuery(window).keyup(function () {\n" +
+    "doShadow();\n" +
+"});\n";
 
-        "/* Add event handlers */\n" +
-        "jQuery(document).ready(function () {\n" +
-            "viewportNavigationExists();\n" +
-        "});\n" +
-        "jQuery(window).click(function () {\n" +
-            "doShadow();\n" +
-        "});\n" +
-        "jQuery(window).scroll(function () {\n" +
-            "doShadow();\n" +
-        "});\n" +
-        "jQuery(window).keydown(function () {\n" +
-            "doShadow();\n" +
-        "});\n" +
-        "jQuery(window).keyup(function () {\n" +
-            "doShadow();\n" +
-    "});\n";
 var script = document.createElement('script');
 var code = document.createTextNode(shadowJss);
 script.appendChild(code);
