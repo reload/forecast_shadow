4200d79965cf5ffb06372a0c06325249f673d600
Simon Larsson <simon@reload.dk>
2017-02-20 11:37:27 +0100

    Use real javascript.

--- shadow.js
+++ shadow.js
@@ -1,60 +1,54 @@
-var shadowJss =
-" /* Find all assignments & views. */ \n" +
-"function doShadow() {\n" +
-    "var forecastUrl = 'forecastapp.com';\n" +
-    "if (jQuery('body').hasClass('ember-application') && (window.location.href.indexOf(forecastUrl) > -1 )) {\n" +
-        "$('.ember-view').each(function() {\n" +
-            "/* Gray the assignment if the assignment title contains our keyword. */\n" +
-            "if (jQuery(this).is('div')\n" + 
-                "&& jQuery(this).hasClass('assignment')\n" +
-                "&& jQuery(this).hasClass('has-notes')\n" + 
-                "&& jQuery(this).filter('[title*=\"shadow\"]')\n" + 
-            ") {\n" +
-                "jQuery(this).removeClass('gray orange red green aqua blue purple magenta');\n" +
-                "jQuery($(this)).addClass('gray');\n" +
-            "};\n" +
-        "});\n" +
-    "}\n" +
-"}\n" +
-"/**\n" +
-" * Adding onClick handler only works when the element exists.\n" +
-" * This function makes sure the handler is not set, before +\n" +
-" * the control-button exists.\n" +
-" */\n" +
-"function viewportNavigationExists() {\n" +
-    "if (jQuery('.row-expand-icon').length == 0) {\n" +
-        "setTimeout(viewportNavigationExists, 800);\n" +
-    "}\n" +
-    "else {\n" +
-        "jQuery('.row-expand-icon').click(function () {\n" +
-            "doShadow();\n" +
-        "});\n" +
-        "doShadow();\n" +
-    "}\n" +
-"}\n" +
-"/* Add event handlers */\n" +
-"jQuery(document).ready(function () {\n" +
-    "viewportNavigationExists();\n" +
-"});\n" +
-"jQuery(window).click(function () {\n" +
-    "doShadow();\n" +
-"});\n" +
-"jQuery(window).scroll(function () {\n" +
-    "doShadow();\n" +
-"});\n" +
-"jQuery(window).keydown(function () {\n" +
-    "doShadow();\n" +
-"});\n" +
-"jQuery(window).keyup(function () {\n" +
-    "doShadow();\n" +
-"});\n";
+/**
+ * @file
+ * Forecast shadow bookings.
+ */
 
+ (function ($) {
+  'use strict';
+  /* Find all assignments & views. */
+  function doShadow() {
+    var forecastUrl = 'forecastapp.com';
+    if (jQuery('body').hasClass('ember-application') && (window.location.href.indexOf(forecastUrl) > -1)) {
+      $('.ember-view').each(function () {
+        /* Gray the assignment if the assignment title contains our keyword. */
+        if (
+          jQuery(this).is('div') &&
+          jQuery(this).hasClass('assignment') &&
+          jQuery(this).hasClass('has-notes') &&
+          jQuery(this).filter('[title*=\"shadow\"]')
+        ) {
+          jQuery(this).removeClass('gray orange red green aqua blue purple magenta');
+          jQuery($(this)).addClass('gray');
+        }
+      });
+    }
+  }
 
-var jq = document.createElement('script');
-jq.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js');
-(document.body || document.head).appendChild(jq);
+  /**
+   * Check for viewport navigation.
+   *
+   * Adding onClick handler only works when the element exists.
+   * This function makes sure the handler is not set, before +
+   * the control-button exists.
+   */
+  function viewportNavigationExists() {
+    if (jQuery('.row-expand-icon').length == 0) {
+      window.setTimeout(viewportNavigationExists, 800);
+    }
+    else {
+      jQuery('.row-expand-icon').click(function () {
+        doShadow();
+      });
+      doShadow();
+    }
+  }
 
-var script = document.createElement('script');
-var code = document.createTextNode(shadowJss);
-script.appendChild(code);
-(document.body || document.head).appendChild(script);
+  /* Add event handlers */
+  $(document).ready(function () {
+    viewportNavigationExists();
+  });
+
+  $(window).on('click scroll keydown keyup', function () {
+    doShadow();
+  });
+})(jQuery);
