dfd25dcdd7a12ae7f089466c7a4041e856f02ac2
Simon Larsson <simon@reload.dk>
2017-02-20 17:51:47 +0100

    Babel all the things!!

---
+++ src/app.js
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * Forecast shadow bookings.
+ */
+
+'use strict';
+
+class Shadow {
+  constructor(domNode) {
+    this.domNode = domNode;
+  }
+
+  // Throttling to prevent event mayhem.
+  throttle(callback, wait, context = this) {
+    let timeout = null;
+    let callbackArgs = null;
+
+    const later = () => {
+      callback.apply(context, callbackArgs);
+      timeout = null;
+    }
+
+    return () => {
+      if (!timeout) {
+        callbackArgs = arguments;
+        timeout = setTimeout(later, wait);
+      }
+    }
+  }
+
+  // Find and alter shadow bookings.
+  doShadows() {
+    const forecastUrl = 'forecastapp.com';
+    const bodyClasses = document.querySelector('body').className;
+    const checkClass = new RegExp('ember-application');
+    const isApp = checkClass.test(bodyClasses);
+    if (isApp && (window.location.href.indexOf(forecastUrl) > -1)) {
+      const assignments = document.querySelectorAll('.ember-view.assignment.has-notes');
+      if (assignments.length > 0) {
+        assignments.forEach((assignment) => {
+          assignment.classList.remove('gray', 'orange', 'red', 'green', 'aqua', 'blue', 'purple', 'magenta');
+          assignment.classList.add('gray');
+        });
+      }
+    }
+  }
+
+  // Check for dom changes.
+  observe() {
+    const targetNode = this.domNode;
+    const observerConfig = {
+      attributes: false,
+      childList: true,
+      characterData: true,
+      subtree: true,
+    };
+    const self = this;
+
+    return new Promise((resolve) => {
+      const observer = new MutationObserver(function (mutations) {
+        // For the sake of...observation...let's output the mutation to console to see how this all works.
+        mutations.forEach((mutation) => {
+          self.throttle({
+            self.doShadows();
+            console.log(mutation.type);
+          }, 500);
+        });
+        resolve(mutations);
+      });
+      observer.observe(targetNode, observerConfig);
+    });
+  }
+}
+
+// Start observing.
+const shadowBookings = new Shadow(document.body);
+shadowBookings.observe();
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+module.exports = {
+  entry: './src/app.js',
+  output: { filename: 'app.js', path: 'dist' },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [ 'es2015', { modules: false } ]
+          ]
+        }
+      }
+    ]
+  }
+};
